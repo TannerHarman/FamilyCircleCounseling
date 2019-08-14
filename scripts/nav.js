8727d7409b2e3503f6eb87c9e8d8df2af047c9c6
Tanner Harman <harmtan2@gmail.com>
2019-08-14 16:35:25 -0600

    Added transition to menu as it hides & unhides

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,6 @@
 const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('#menu-items');
+const menu = document.querySelector('.menu-items');
 
 menuButton.addEventListener('click', () => {
-  menu.classList == 'menu-hide' ? menu.classList = 'menu-show' : menu.classList = 'menu-hide';
+  menu.id == 'menu-hide' || menu.id == 'menu' ? menu.id = 'menu-show' : menu.id = 'menu-hide';
 });
