0f5960ca3da8ba9f60900dccbe8d5d6ef4ccd7cc
Tanner Harman <harmtan2@gmail.com>
2019-08-15 17:20:47 -0600

    Added hover affect for phone & email mobile nav & the link points to the contact page

--- scripts/nav.js
+++ scripts/nav.js
@@ -9,9 +9,18 @@ let setPhoneLink = () => {
   let android = navigator.userAgent.match(/Android/i);
   let iphone = navigator.userAgent.match(/iPhone/i)
   const phoneLink = document.querySelector('#phone');
+  const emailLink = document.querySelector('#email');
 
   if (android || iphone) {
     phoneLink.href = 'tel:+1-307-347-4001';
+  } else if (location.pathname == "/index.html") {
+    phoneLink.href = email.Link = 'pages/contact.html';
+    phoneLink.classList.add('hover-info');
+    emailLink.classList.add('hover-info');
+  } else {
+    phoneLink.href = email.Link = 'contact.html';
+    phoneLink.classList.add('hover-info');
+    emailLink.classList.add('hover-info');
   }
 }
 
