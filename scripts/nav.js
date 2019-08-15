df7cc12739e6241779a674c18ece0d7914e51169
Tanner Harman <harmtan2@gmail.com>
2019-08-14 21:51:09 -0600

    Testing Android Phone Link

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,3 +5,25 @@ menuButton.addEventListener('click', () => {
   menu.id == 'menu-hide' || menu.id == 'menu' ? menu.id = 'menu-show' : menu.id = 'menu-hide';
 });
 
+let getOS = () => {
+  let OSName = 'Unknown OS'
+
+  if (navigator.platform.indexOf('Win')!=-1) OSName='Windows';
+  if (navigator.platform.indexOf('Mac')!=-1) OSName='MacOS';
+  if (navigator.platform.indexOf('X11')!=-1) OSName='UNIX';
+  if (navigator.platform.indexOf('Linux')!=-1) OSName='Linux';
+  if (navigator.platform.indexOf('iPhone')!=-1) OSName='iOS';
+  if (navigator.platform.indexOf('Android')!=-1) OSName='Android';
+
+  return OSName;
+}
+
+let setPhoneLink = (getOS) => {
+  let phoneLink = document.querySelector('#phone');
+
+  let OS = getOS();
+
+  if (OS == 'Android' || OS == 'iOS') {
+    phoneLink.href = 'tel:+1-307-347-4001'
+  }
+}
