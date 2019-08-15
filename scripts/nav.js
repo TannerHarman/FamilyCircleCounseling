8328d7d0da2ffa464bbb2c9ef6808d315b0468c1
Tanner Harman <harmtan2@gmail.com>
2019-08-14 20:45:28 -0600

    Did not work. Will attempt at later date.

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,11 +3,5 @@ const menu = document.querySelector('.menu-items');
 
 menuButton.addEventListener('click', () => {
   menu.id == 'menu-hide' || menu.id == 'menu' ? menu.id = 'menu-show' : menu.id = 'menu-hide';
-
-  if (menu.id === 'menu-show') {
-    menuButton.id = 'menu-button-active';
-  } else if (menu.id === 'menu-hide') {
-    menuButton.id = '';
-  }
 });
 
