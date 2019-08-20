47c0bbecd60817687d7f70c8f14d69ae21b95ea5
Tanner Harman <harmtan2@gmail.com>
2019-08-20 08:44:57 -0600

    Got background hover change working on Desktop

--- scripts/services.js
+++ scripts/services.js
@@ -1,22 +1,35 @@
 const invites = document.querySelectorAll('.invite');
 const touchDevice = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i);
 
-console.log(touchDevice);
-
 if (touchDevice) {
   invites.forEach(invite => {
     invite.addEventListener('click', () => {
       invite.classList.toggle('active');
+      if (parentElement.classList.contains('couples-program')){
+        invite.parentElement.classList.toggle('couples-program-active')
+      } else {
+        invite.parentElement.classList.toggle('grief-program-active')
+        }
     });
   });
 } else {
   invites.forEach(invite => {
     invite.addEventListener('mouseover', () => {
-      invite.classList.add('active');
+      if (invite.parentElement.classList.contains('couples-program')) {
+        invite.parentElement.classList.add('couples-program-active');
+      } else {
+        invite.parentElement.classList.add('grief-program-active');
+      }
     });
   
     invite.addEventListener('mouseout', () => {
-      invite.classList = 'invite';
+      invite.classList.remove('active');
+      if (invite.parentElement.classList.contains('couples-program-active')) {
+        invite.parentElement.classList.remove('couples-program-active');
+      } else {
+        invite.parentElement.classList.remove('grief-program-active')
+      }
+      
     });
   });
 }
